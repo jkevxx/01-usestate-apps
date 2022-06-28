1394a9219cbf980d1b6fc7b42aaf1ad98a684aa3
Kevin Paez <kevin.paez1010@gmail.com>
2022-06-28 16:14:29 -0500

    Shopping Cart App completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import ShoppingCartApp from './components/5ShoppingCart/ShoppingCartApp';
 // import TodoApp from './components/todo/TodoApp';
 
 // import Counter from './components/1Counter/CounterApp';
 // import ConditionalApp from './components/2Conditional/ConditionalApp';
 // import ErrorApp from './components/3Error/ErrorApp';
-import ProductApp from './components/4Product/ProductApp';
+// import ProductApp from './components/4Product/ProductApp';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
       {/* <Counter /> */}
       {/* <ConditionalApp /> */}
       {/* <ErrorApp /> */}
-      <ProductApp />
+      {/* <ProductApp /> */}
+      {<ShoppingCartApp />}
     </div>
   );
 }
--- src/components/5ShoppingCart/ShoppingCartApp.jsx
+++ src/components/5ShoppingCart/ShoppingCartApp.jsx
@@ -1,9 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialCart = [
+  { id: 1, title: "Product 1", description: "Desc 1" },
+  { id: 2, title: "Product 2", description: "Desc 2" },
+];
 
 function ShoppingCartApp() {
+  const [cart, setCart] = useState(initialCart);
+
+  const deleteProduct = (productId) => {
+    console.log(productId);
+    const changeCart = cart.filter((product) => product.id !== productId);
+    setCart(changeCart);
+  };
+
+  const addProduct = (newProduct) => {
+    newProduct.id = Date.now();
+    //if you want to add first newProduct change order [...cart, newProduct] otherwise [newProduct, ...cart]
+    const changedCart = [newProduct, ...cart];
+    setCart(changedCart);
+    console.log(newProduct);
+  };
+
+  const updateProduct = (editProduct) => {
+    const changedCart = cart.map((product) =>
+      product.id === editProduct.id ? editProduct : product
+    );
+    setCart(changedCart);
+  };
+
   return (
     <div>
-      <span>ShoppingCartApp</span>
+      <button
+        onClick={() =>
+          addProduct({ title: "Product 3", description: "Desc 3" })
+        }
+      >
+        Add
+      </button>
+
+      <button
+        onClick={() =>
+          updateProduct({
+            id: 1,
+            title: "New title",
+            description: "new description",
+          })
+        }
+      >
+        Update
+      </button>
+
+      {cart.map((product) => (
+        <div key={product.id}>
+          <p>{product.title}</p>
+          <p>{product.description}</p>
+          <button onClick={() => deleteProduct(product.id)}>Delete</button>
+          <br />
+        </div>
+      ))}
+
+      <br />
+      <br />
+      <pre>{JSON.stringify(cart, null, 2)}</pre>
     </div>
   );
 }
